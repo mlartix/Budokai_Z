6b0f30f65bb620d25d77b0349ff3351e097ece28
Tee Hee <74879660+mlartix@users.noreply.github.com>
2023-04-03 23:58:29 -0500

    Update Player_ESP.lua

--- Player_ESP.lua
+++ Player_ESP.lua
@@ -1,9 +1,5 @@
-pcall(function()
-    getgenv().Connections.Stepped:Disconnect()
-    getgenv().Connections.Stepped = nil
-end)
-
 local RefreshRate = 14
+
 getgenv().ESPToggle = true
 getgenv().Connections = {
     RenderStepped = nil,
@@ -17,11 +13,11 @@ function WTS(part)
     return Vector2.new(screen.x, screen.y), screen.z
 end
     
-function ESP(player)
+function ESP(hrp)
     local LastRefresh = 0
   --  "["..math.floor(player.Character.Humanoid.Health + .05)"/"math.floor(player.Character.Humanoid.MaxHealth + .05))"]"
     local name = Drawing.new("Text")
-    name.Text = player.Name
+    name.Text = hrp.Parent.Name
     name.Color = Color3.fromHSV(tick() * 24 % 255/255, 1, 1)
     --name.Position = WTS(player.Character.HumanoidRootPart)
     name.Size = 20.0
@@ -33,18 +29,15 @@ function ESP(player)
         if (tick() - LastRefresh) > (RefreshRate / 1000) then
             LastRefresh = tick();
             pcall(function()
-                local destroyed = player.Character.HumanoidRootPart:IsDescendantOf(workspace)
-                if not destroyed and name ~= nil then
+                local destroyed = not hrp:IsDescendantOf(workspace)
+                if destroyed and name ~= nil then
                     name:Remove()
                 end
-                if player.Character.HumanoidRootPart ~= nil then
-                    name.Position = WTS(player.Character.HumanoidRootPart)
-                end
-                local _, screen = workspace.CurrentCamera:WorldToViewportPoint(player.Character.HumanoidRootPart.Position)
+                local _, screen = workspace.CurrentCamera:WorldToViewportPoint(hrp.Position)
                 if screen and getgenv().ESPToggle then
  
-                    name.Text = string.format("%s [%d/%d]\n%d [%d]", player.Name, math.floor(player.Character.Humanoid.Health + .05), math.floor(player.Character.Humanoid.MaxHealth + .05), select(2, WTS(player.Character.HumanoidRootPart)), player.Character.Core.PowerLevel.Value)
-                    name.Position = WTS(player.Character.HumanoidRootPart)
+                    name.Text = string.format("%s [%d/%d]\n%d [%d]", hrp.Parent.Name, math.floor(hrp.Parent.Humanoid.Health + .05), math.floor(hrp.Parent.Humanoid.MaxHealth + .05), select(2, WTS(hrp)), hrp.Parent.Core.PowerLevel.Value)
+                    name.Position = WTS(hrp)
                     name.Visible = true
                     name.Color = Color3.fromHSV(tick() * 24 % 255/255, 1, 1)
                 else
@@ -56,14 +49,16 @@ function ESP(player)
 end
 
 for _,v in pairs(game.Players:GetPlayers()) do
-    if v ~= game.Players.LocalPlayer then
-        ESP(v)
+    if v ~= game.Players.LocalPlayer and v.Character and v.Character:FindFirstChild("HumanoidRootPart") then
+        ESP(v.Character.HumanoidRootPart)
     end
 end
 
 game.Players.PlayerAdded:Connect(function(player)
-    player.CharacterAdded:Connect(function(character)
-        ESP(player)
+    if player == game.Players.LocalPlayer then return end;
+    player.CharacterAdded:Connect(function(v)
+        local hrp = v:WaitForChild("HumanoidRootPart")
+        ESP(hrp)
     end)
 end)
 
@@ -71,7 +66,7 @@ game:GetService("UserInputService").InputBegan:Connect(function(input, gameProce
     --if gameProcessed then return end
     
     if input.KeyCode == Enum.KeyCode[string.upper(tostring(_G.Keybind))] then
-        if getgenv().ESPToggle then
+        if _G.Keybind then
             getgenv().ESPToggle = false
         else
             getgenv().ESPToggle = true
